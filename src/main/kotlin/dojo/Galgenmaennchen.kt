4891aa277f272fc75b7583f489eceb031d0d2891
fwolf <f.wolf@neusta.de>
2020-10-30 17:37:16 +0100

    init Galgenmännchen-Dojo

---
+++ src/main/kotlin/dojo/Galgenmaennchen.kt
@@ -0,0 +1,16 @@
+package dojo
+
+class Galgenmaennchen(gesuchtesWort: String) {
+
+    /*
+     * 1. Prüfe an welcher Stelle des gesuchten Worts das Zeichen vorkommt.
+     * 2. Gebe die bisherigen erratenen Bestandteile des Worts
+     * in der Form "--x--xy-" zurück.
+     */
+    fun rateBuchstabe(buchstabe:Char): String{
+        return ""
+    }
+
+
+
+}
---
+++ src/test/kotlin/dojo/GalgenmaennchenTest.kt
@@ -0,0 +1,11 @@
+package dojo
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+
+internal class GalgenmaennchenTest{
+
+
+}
+
+
