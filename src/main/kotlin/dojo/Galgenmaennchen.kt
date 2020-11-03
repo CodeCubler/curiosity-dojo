b88bcdc97c75e083d313395e8b269b6b17eb10cd
fwolf <f.wolf@neusta.de>
2020-11-03 11:01:10 +0100

    first dojo

--- src/main/kotlin/dojo/Galgenmaennchen.kt
+++ src/main/kotlin/dojo/Galgenmaennchen.kt
@@ -1,16 +1,27 @@
 package dojo
 
-class Galgenmaennchen(gesuchtesWort: String) {
+import java.lang.StringBuilder
+
+class Galgenmaennchen(val gesuchtesWort: String) {
 
     /*
      * 1. Prüfe an welcher Stelle des gesuchten Worts das Zeichen vorkommt.
      * 2. Gebe die bisherigen erratenen Bestandteile des Worts
      * in der Form "--x--xy-" zurück.
      */
-    fun rateBuchstabe(buchstabe:Char): String{
-        return ""
-    }
 
+    //
+    fun rateBuchstabe(buchstabe: Char): String {
+        val sb = StringBuilder()
+        for (i in 0 until gesuchtesWort.length) {
+            if (gesuchtesWort[i] == buchstabe) {
+                sb.append(buchstabe)
+            } else {
+                sb.append("-")
+            }
+        }
+        return sb.toString()
+    }
 
 
 }
--- src/test/kotlin/dojo/GalgenmaennchenTest.kt
+++ src/test/kotlin/dojo/GalgenmaennchenTest.kt
@@ -1,10 +1,27 @@
 package dojo
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DisplayName
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
 internal class GalgenmaennchenTest{
 
+    @Test
+    fun `test with wrong character`() {
+        assertThat(Galgenmaennchen("programm").rateBuchstabe('x')).isEqualTo("--------")
+    }
+
+    @Test
+    fun `test with containing character`() {
+        assertThat(Galgenmaennchen("programm").rateBuchstabe('r')).isEqualTo("-r--r---")
+    }
+
+    @Test
+    fun `test with second containing character`() {
+        assertThat(Galgenmaennchen("programm")
+            .rateBuchstabe('r')
+            .rateBuchstabe('o'))
+            .isEqualTo("-ro-r---")
+    }
 
 }
 
