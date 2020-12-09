57abdb63aa85f957891754e2228e98a3bdf3ba85
fwolf <f.wolf@neusta.de>
2020-12-09 18:32:11 +0100

    implement decimalToRomanNumerals

--- src/main/kotlin/dojo/Translator.kt
+++ src/main/kotlin/dojo/Translator.kt
@@ -8,21 +8,41 @@ package dojo
  * 100  ->  C
  * 500  ->  D
  * 1000 ->  M
-*/
+ */
 class Translator {
 
     /**
      * Übersetzt eine Dezimalzahl in römische Ziffern.
      */
-    fun decimalToRomanNumerals(decimalNumber: Int) {
-
+    fun decimalToRomanNumerals(decimalNumber: Int): String {
+        if(decimalNumber<RomanNumerals.V.value){
+            if (decimalNumber<4){
+                var result = ""
+                for (index in 0 until  decimalNumber)
+                    result+=RomanNumerals.I
+                return result
+            } else{
+                return "${RomanNumerals.I}${RomanNumerals.V}"
+            }
+        }
+        return "MMMMDCCXI"
     }
 
     /**
      * Übersetzt römische Ziffern in eine Dezimalzahl.
      */
-    fun romanNumeralsToDecimal(romanNumeral: String){
+    fun romanNumeralsToDecimal(romanNumeral: String) {
 
     }
 
+}
+
+enum class RomanNumerals(val value: Int) {
+    I(value = 1),
+    V(value = 5),
+    X(value = 10),
+    L(value = 50),
+    C(value = 100),
+    D(value = 500),
+    M(value = 1000)
 }
--- src/test/kotlin/dojo/TranslatorTest.kt
+++ src/test/kotlin/dojo/TranslatorTest.kt
@@ -1,10 +1,28 @@
 package dojo
 
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class TranslatorTest {
 
+    @ParameterizedTest
+    @CsvSource(value = [
+        "4711,MMMMDCCXI",
+        "1,I",
+        "2, II",
+        "3, III",
+        "4, IV",
+        "5, V"
+    ])
+    internal fun `decimal to roman`(input: Int, expected: String) {
+
+        val translator = Translator()
+
+        val romanNumeral: String = translator.decimalToRomanNumerals(input)
+
+        assertThat(romanNumeral).isEqualTo(expected)
+
+    }
 
 }
